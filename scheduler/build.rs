d9bd17d340d4639eda896b5a24ecaf9153bb8ea8
Protocole <burgoni.alexandre@protonmail.com>
2021-06-08 18:23:44 +0200

    wip: scheduler

--- scheduler/build.rs
+++ scheduler/build.rs
@@ -1,4 +1,6 @@
-fn main() {
-    tonic_build::compile_protos("../proto/controller.proto").unwrap();
-    tonic_build::compile_protos("../proto/worker.proto").unwrap();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::compile_protos("../proto/controller.proto")?;
+    tonic_build::compile_protos("google/protobuf/empty.proto")?;
+    tonic_build::compile_protos("../proto/worker.proto")?;
+    Ok(())
 }
--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -1,3 +1,15 @@
+use tonic::{transport::Server, Request, Response, Status};
+use common::{ResourceStatus, WorkerMetric, InstanceMetric, WorkerStatus, Workload};
+use worker::worker_server::{Worker, WorkerServer};
+use protobuf::well_known_types::Empty;
+use tokio_stream::wrappers::ReceiverStream;
+use tokio::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+use std::thread::sleep;
+
+// Common is needed to be included, as controller & worker
+// are using it
 pub mod common {
     tonic::include_proto!("common");
 }
@@ -10,6 +22,51 @@ pub mod worker {
     tonic::include_proto!("worker");
 }
 
-fn main() {
+#[derive(Debug)]
+pub struct WorkerService;
+
+#[tonic::async_trait]
+impl Worker for WorkerService {
+    type RegisterStream = ReceiverStream<Result<Workload, Status>>;
+
+    async fn register(
+        &self,
+        _request: Request<()>,
+    ) -> Result<Response<Self::RegisterStream>, Status> {
+        let (mut tx, rx) = mpsc::channel(1024);
+        tokio::spawn(async move {
+            for feature in 1..100 {
+                tx.send(Ok(Workload {
+                    name: String::from(format!("{} test", feature)),
+                    tag: String::from(format!("{} test", feature)),
+                    image: String::from(format!("{} test", feature)),
+                })).await.unwrap();
+                sleep(Duration::new(1,10));
+                println!("Sending now");
+            }
+        });
+
+        Ok(Response::new(ReceiverStream::new(rx)))
+    }
+
+    async fn send_status_updates(
+        &self,
+        _request: Request<tonic::Streaming<WorkerStatus>>,
+    ) -> Result<Response<()>, Status> {
+        unimplemented!()
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
+    let service = WorkerService {};
+    let server = WorkerServer::new(service);
+
+    Server::builder()
+        .add_service(server)
+        .serve("127.0.0.1:8081".parse().unwrap())
+        .await?;
+
+    Ok(())
 }
