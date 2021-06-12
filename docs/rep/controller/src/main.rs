06a780d2a2cf0051f37bbd0d988d28f3ed8ead01
Malo <malo.polese@gmail.com>
2021-06-12 20:50:23 +0200

    WIP

--- docs/rep/controller/src/main.rs
+++ docs/rep/controller/src/main.rs
@@ -1,24 +1,37 @@
-use std::thread;
-
 mod api;
 mod tools;
+
+use std::thread;
+use std::sync::mpsc::channel;
 
 use api::{external::ExternalAPI, internal::InternalAPI};
 use tools::Logger;
 
-fn main() {
-    let logger = Logger::new(String::from("Main"));
-    let internal_api = InternalAPI::new(&logger);
-    let external_api = ExternalAPI::new(&logger);
+struct Gate {
+    logger: Sender,
+    
+}
 
-    thread::spawn(move || {
+fn main() {
+    let (logging_sender, logging_receiver) = channel();
+    
+    let logger = Logger::new(logging_receiver, String::from("Main"));
+    let internal_api = InternalAPI::new(logging_sender);
+    let external_api = ExternalAPI::new(logging_sender);
+    
+    let mut threads = Vec::new();
+
+    threads.push(thread::spawn(move || {
         internal_api.run();
-    })
-    .join()
-    .unwrap();
-    thread::spawn(move || {
+    }))
+    threads.push(thread::spawn(move || {
         external_api.run();
-    })
-    .join()
-    .unwrap();
+    }))
+    threads.push(thread::spawn(move || {
+        logger.run();
+    }))
+
+    for thread in threads {
+        thread.join().unwrap();
+    }
 }
