b651993b3cc94dc2a3109fdb3598484f049493e0
Protocole <burgoni.alexandre@protonmail.com>
2021-06-30 18:22:11 +0200

    feat: add multi workers

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -1,18 +1,29 @@
 use proto::common::{WorkerStatus, Workload};
-use std::error::Error;
-use std::fmt;
 use std::net::SocketAddr;
 use tokio::sync::mpsc::Sender;
 use tonic::Status;
+use std::error::Error;
+use std::fmt;
+use std::collections::HashMap;
 
 /// Define the structure of message send through the channel between
 /// the manager and a worker
 pub type WorkloadChannelType = Result<Workload, Status>;
 
+/// The state is shared across multiple threads (Manager & Scheduler)
+/// so it is necessary to have a smart pointer & its mutex
+pub type StateType = HashMap<String, WorkloadInstance>;
+
 #[derive(Debug)]
 pub enum Event {
+    /// Workers register to the Scheduler so they can serve
+    /// the cluster
     Register(Sender<WorkloadChannelType>, SocketAddr),
-    Schedule(Workload),
+    /// Controller can send workload, we use the verb Schedule to describe
+    /// this event
+    ScheduleRequest(Workload),
+    /// This is meant for a controller subscription event
+    /// Controller subscribe to the scheduler in order to get updqtes
     Subscribe(Sender<Result<WorkerStatus, Status>>, SocketAddr),
 }
 
@@ -21,6 +32,14 @@ pub enum SchedulerError {
     ClusterFull,
     RegistrationFailed(String),
 }
+
+impl fmt::Display for SchedulerError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl Error for SchedulerError {}
 
 
 #[derive(Debug)]
@@ -38,7 +57,7 @@ impl Controller {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Worker {
     /// Unique ID for the worker, only used internally for now
     pub id: u8,
@@ -48,19 +67,75 @@ pub struct Worker {
     ///
     /// The following code is used in order to schedule an instance
     /// ```
+    /// use rik_scheduler::{Worker, WorkloadChannelType};
+    /// use proto::common::{Workload};
+    /// use tokio::sync::mpsc::{channel, Receiver, Sender};
+    /// use std::net::{SocketAddr, IpAddr, Ipv4Addr};
+    /// let (sender, receiver) = channel::<WorkloadChannelType>(1024);
+    /// let worker = Worker {
+    ///     id: 0,
+    ///     channel: sender,
+    ///     addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+    /// }
     /// worker.channel.send(Ok(Workload {
     ///     instance_id: String::from("testing"),
     ///     definition: String::from("{}"),
     /// })).await?;
     /// ```
-    // TODO: Create a trait to send data
-    pub channel: Sender<Result<Workload, Status>>,
+    pub channel: Sender<WorkloadChannelType>,
     /// Remote addr of the worker
     pub addr: SocketAddr,
 }
 
 impl Worker {
-    pub fn new(id: u8, channel: Sender<Result<Workload, Status>>, addr: SocketAddr) -> Worker {
+    pub fn new(id: u8, channel: Sender<WorkloadChannelType>, addr: SocketAddr) -> Worker {
         Worker { id, channel, addr }
     }
 }
+
+#[tonic::async_trait]
+pub trait Send<T> {
+    async fn send(&self, data: T) -> Result<(), Status>;
+}
+
+#[derive(Debug, Clone)]
+pub struct WorkloadInstance {
+    worker_id: Option<u8>,
+    workload: Workload,
+}
+
+impl WorkloadInstance {
+    pub fn new(workload: Workload, worker: Option<Worker>) -> WorkloadInstance {
+        WorkloadInstance {
+            workload,
+            worker_id: match worker {
+                Some(worker) => Some(worker.id),
+                _ => None,
+            },
+        }
+    }
+
+    pub fn set_worker(&mut self, id: u8) {
+        self.worker_id = Some(id);
+    }
+
+    pub fn has_worker(&self) -> bool {
+        match self.worker_id {
+            Some(_) => true,
+            _ => false
+        }
+    }
+
+    pub fn get_worker_id(&self) -> Option<u8> {
+        self.worker_id
+    }
+
+    pub fn get_instance_id(&self) -> String {
+        self.workload.instance_id.clone()
+    }
+
+    pub fn get_workload(self) -> Workload {
+        self.workload
+    }
+}
+
--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -1,26 +1,53 @@
 use env_logger::Env;
-use log::{error, info};
+use log::{error, info, debug};
 use proto::common::{WorkerStatus, Workload};
 use proto::controller::controller_server::{
     Controller as ControllerClient, ControllerServer,
 };
-use proto::protobuf::well_known_types::Empty;
-use rik_scheduler::{Controller, Worker};
+use rik_scheduler::{Controller, Worker, Send, SchedulerError, WorkloadInstance, StateType};
 use proto::worker::worker_server::{Worker as WorkerClient, WorkerServer};
-use rik_scheduler::{Event, SchedulerError, WorkloadChannelType};
+use rik_scheduler::{Event, WorkloadChannelType};
 use std::default::Default;
 use std::net::SocketAddr;
 use tokio::sync::mpsc::{channel, Receiver, Sender};
 use tokio_stream::wrappers::ReceiverStream;
 use tokio_stream::StreamExt;
 use tonic::{transport::Server, Code, Request, Response, Status};
+use std::collections::{HashMap};
+use tokio::sync::mpsc::error::SendError;
+use rand::seq::SliceRandom;
 
 #[derive(Debug, Clone)]
 pub struct GRPCService {
     /// Channel used in order to communicate with the main thread
     /// In the case the worker doesn't know its ID yet, put 0 in the first
     /// item of the tuple
-    pub sender: Sender<Event>,
+    sender: Sender<Event>,
+}
+
+impl GRPCService {
+    fn new(sender: Sender<Event>) -> GRPCService {
+        GRPCService {
+            sender
+        }
+    }
+}
+
+#[tonic::async_trait]
+impl Send<Event> for GRPCService {
+    async fn send(&self, data: Event) -> Result<(), Status> {
+        self.sender
+            .send(data)
+            .await
+            .map_err(|e| {
+                error!("Failed to send message from gRPCService to Manager, error: {}", e);
+                Status::new(
+                    Code::Unavailable,
+                    "We cannot process your request at this time"
+                )
+            })
+
+    }
 }
 
 #[tonic::async_trait]
@@ -34,16 +61,7 @@ impl WorkerClient for GRPCService {
         // Streaming channel that sends workloads
         let (stream_tx, stream_rx) = channel::<WorkloadChannelType>(1024);
         let addr = _request.remote_addr().expect("No remote address found");
-
-        self.sender
-            .send(Event::Register(stream_tx, addr))
-            .await
-            .map_err(|_| {
-                Status::new(
-                    Code::Unavailable,
-                    "Worker service cannot process your request",
-                )
-            })?;
+        self.send(Event::Register(stream_tx, addr)).await?;
 
         Ok(Response::new(ReceiverStream::new(stream_rx)))
     }
@@ -73,15 +91,7 @@ impl WorkerClient for GRPCService {
 #[tonic::async_trait]
 impl ControllerClient for GRPCService {
     async fn schedule_instance(&self, _request: Request<Workload>) -> Result<Response<()>, Status> {
-        self.sender
-            .send(Event::Schedule(_request.get_ref().clone()))
-            .await
-            .map_err(|_| {
-                Status::new(
-                    Code::Unavailable,
-                    "Controller service cannot process your request",
-                )
-            })?;
+        self.send(Event::ScheduleRequest(_request.get_ref().clone())).await?;
 
         Ok(Response::new(()))
     }
@@ -94,15 +104,7 @@ impl ControllerClient for GRPCService {
     ) -> Result<Response<Self::GetStatusUpdatesStream>, Status> {
         let (stream_tx, stream_rx) = channel::<Result<WorkerStatus, Status>>(1024);
         let addr = _request.remote_addr().expect("No remote address found");
-        self.sender
-            .send(Event::Subscribe(stream_tx, addr))
-            .await
-            .map_err(|_| {
-                Status::new(
-                    Code::Unavailable,
-                    "Controller service cannot process your request",
-                )
-            })?;
+        self.send(Event::Subscribe(stream_tx, addr)).await?;
 
         Ok(Response::new(ReceiverStream::new(stream_rx)))
     }
@@ -114,6 +116,8 @@ pub struct Manager {
     channel: Receiver<Event>,
     controller: Option<Controller>,
     worker_increment: u8,
+    state: StateType,
+    expected_state: StateType
 }
 
 
@@ -125,20 +129,23 @@ impl Manager {
             channel: receiver,
             controller: None,
             worker_increment: 0,
+            state: HashMap::new(),
+            expected_state: HashMap::new(),
         };
         instance.run_workers_listener(sender.clone());
         instance.run_controllers_listener(sender.clone());
+        //instance.run_scheduler(sender.clone(), instance.workers.clone());
         let channel_listener = instance.listen();
         channel_listener.await?;
         Ok(instance)
     }
 
     fn run_workers_listener(&self, sender: Sender<Event>) {
-        let server = WorkerServer::new(GRPCService { sender });
+        let server = WorkerServer::new(GRPCService::new(sender));
         tokio::spawn(async move {
             let server = Server::builder()
                 .add_service(server)
-                .serve("127.0.0.1:8081".parse().unwrap());
+                .serve("127.0.0.1:4995".parse().unwrap());
 
             info!("Worker gRPC listening thread up");
 
@@ -149,11 +156,11 @@ impl Manager {
     }
 
     fn run_controllers_listener(&self, sender: Sender<Event>) {
-        let server = ControllerServer::new(GRPCService { sender });
+        let server = ControllerServer::new(GRPCService::new(sender));
         tokio::spawn(async move {
             let server = Server::builder()
                 .add_service(server)
-                .serve("127.0.0.1:10000".parse().unwrap());
+                .serve("127.0.0.1:4996".parse().unwrap());
 
             info!("Controller gRPC listening thread up");
 
@@ -167,30 +174,12 @@ impl Manager {
         while let Some(e) = &self.channel.recv().await {
             match e {
                 Event::Register(channel, addr) => {
-                    self.worker_increment += 1;
-                    if self.workers.is_empty() {
-                        self.register(channel.clone(), addr.clone());
-                    } else {
-                        channel
-                            .send(Err(Status::aborted("Cluster is full")))
-                            .await?;
-                    }
-                }
-                Event::Schedule(workload) => {
-                    if self.workers.is_empty() {
-                        error!(
-                            "No worker are registered to schedule the workload: {:?}",
-                            workload
-                        );
-                    } else {
-                        let worker = &self.workers[0];
-                        worker.channel.send(Ok(workload.clone())).await?;
-                        info!(
-                            "A workload was sent to the worker {}: {:?}",
-                            worker.id, workload
-                        );
-                    }
-                }
+                    self.register(channel.clone(), addr.clone())?
+                },
+                Event::ScheduleRequest(workload) => {
+                    debug!("New workload definition received to schedule {}", workload.instance_id);
+                    self.update_expected_state(WorkloadInstance::new(workload.clone(), None)).await;
+                },
                 Event::Subscribe(channel, addr) => {
                     self.controller = Some(Controller::new(channel.clone(), addr.clone()));
                 }
@@ -199,13 +188,86 @@ impl Manager {
         Ok(())
     }
 
-    fn register(&mut self, channel: Sender<WorkloadChannelType>, addr: SocketAddr) {
-        let worker = Worker::new(*&self.worker_increment, channel, addr);
+    fn get_next_id(&mut self) -> Result<u8, SchedulerError> {
+        match self.worker_increment {
+            u8::MAX => Err(SchedulerError::ClusterFull),
+            _ => {
+                self.worker_increment += 1;
+                Ok(self.worker_increment)
+            },
+        }
+    }
+
+    fn register(&mut self, channel: Sender<WorkloadChannelType>, addr: SocketAddr) -> Result<(), SchedulerError> {
+        let worker = Worker::new(self.get_next_id()?, channel, addr);
         info!(
             "Worker with ID {} is now registered, ip: {}",
             worker.id, worker.addr
         );
         self.workers.push(worker);
+        Ok(())
+    }
+
+    async fn schedule(&mut self, workload: WorkloadInstance) -> Result<(), SendError<WorkloadChannelType>> {
+        debug!("[schedule] {:#?}", workload.get_worker_id());
+        if !workload.has_worker() {
+            error!("Tried to schedule workload while no worker assigned");
+            return Ok(());
+        }
+        match self.get_worker_sender(workload.get_worker_id().unwrap()) {
+            Some(sender) => {
+                info!(
+                    "A workload was sent to the worker {}: {:?}",
+                    workload.get_worker_id().unwrap(), workload
+                );
+                sender.send(Ok(workload.get_workload())).await?;
+            },
+            _ => {
+                error!("Tried to schedule workload on a no longer existing worker");
+                return Ok(());
+            }
+        }
+        Ok(())
+    }
+
+    fn get_worker_sender(&self, worker_id: u8) -> Option<Sender<WorkloadChannelType>> {
+        for worker in self.workers.iter() {
+            if worker.id == worker_id {
+                return Some(worker.channel.clone());
+            }
+        }
+
+        None
+    }
+
+    async fn update_expected_state(&mut self, item: WorkloadInstance) {
+        let instance_id = item.get_instance_id();
+        let old_instance = self.expected_state.insert(instance_id.clone(), item);
+
+        match old_instance {
+            Some(_) => debug!("Instance {} updated into the expected state", instance_id),
+            None => debug!("Inserted a new instance into the expected state, id: {}", instance_id)
+        };
+
+        self.scan_diff_state().await;
+    }
+
+    async fn scan_diff_state(&mut self) {
+        for (instance_id, workload) in self.expected_state.clone().iter() {
+            if !self.state.contains_key(instance_id) {
+                info!("Detected diff between expected state & new state, updating with instance_id {}", instance_id);
+                let worker = self.workers.choose(&mut rand::thread_rng());
+                match worker {
+                    Some(worker) => {
+                        let mut workload = workload.clone();
+                        workload.set_worker(worker.id);
+                        self.schedule(workload.clone()).await;
+                        self.state.insert(workload.get_instance_id(), workload);
+                    },
+                    _ => (),
+                };
+            }
+        }
     }
 }
 
