d3f25a65ed594e02b5e7762e5ad9e2b41f781649
Thomas Gouveia <thomas.gouveia@ioterop.com>
2021-07-01 18:43:57 +0200

    fix: remove comments in main.rs

--- riklet/src/main.rs
+++ riklet/src/main.rs
@@ -33,43 +33,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     log::info!("Riklet v{}", VERSION);
 
-    // The path should be set by the top level riklet module, this is just for test purposes.
-    // let mut im = ImageManager::new(config.manager)?;
-
-    // Instanciate our container runtime
-    // let runc = Runc::new(config.runner)?;
-
-    // Pull image from docker hub
-    // let image = im.pull("busybox:latest").await?;
-
-    /*let socket_path = PathBuf::from(String::from(format!("/tmp/{}.sock", image.name)));
-    let console_socket = ConsoleSocket::new(&socket_path).unwrap();
-
-    tokio::spawn(async move {
-        match console_socket.get_listener().as_ref().unwrap().accept().await {
-            Ok((stream, _socket_addr)) => {
-                Box::leak(Box::new(stream));
-            },
-            Err(err) => {
-                error!("Receive PTY master error : {:?}", err)
-            }
-        }
-    });
-
-    runc.run(&image.name, &image.bundle.unwrap(), Some(&CreateArgs {
-        pid_file: None,
-        console_socket: Some(socket_path),
-        no_pivot: false,
-        no_new_keyring: false,
-        detach: true
-    })).await?;
-
-    info!("Riklet initialized.");
-
-    let containers = runc.list().await?;
-
-    debug!("Containers : {:?}", containers);*/
-
     riklet.accept().await?;
 
     Ok(())
